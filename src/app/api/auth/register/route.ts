2b72459c3a13f59a97a46f060f8b427c0857781d
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-18 13:07:01 +0530

    add auth js

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,34 @@
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const { name, email, password } = await req.json();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          message: "Email already exists",
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+    const hashedPass = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ name, email, password: hashedPass });
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Account created successfully",
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
