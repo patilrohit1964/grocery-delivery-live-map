3894b427f740817434ecb7da731f9681dcdedf74
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-21 16:57:40 +0530

    add cloudinary

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+});
