70eff56b838c2f5670c8bd592e0def8db51aad0e
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-21 16:50:16 +0530

    add ui for admin,user,deliverboy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { auth } from "@/auth";
+import AdminDashboard from "@/components/AdminDashboard";
+import DeliveryBoy from "@/components/DeliveryBoy";
 import EditRoleMobile from "@/components/EditRoleMobile";
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import UserDashboard from "@/components/UserDashboard";
 import connectDb from "@/lib/db";
 import User from "@/models/user.model";
 import { redirect } from "next/navigation";
@@ -18,9 +22,17 @@ export default async function Home() {
   if (inComplete) {
     return <EditRoleMobile />;
   }
+  const plainUser = JSON.parse(JSON.stringify(user));
   return (
     <>
-      <Navbar user={user}/>
+      <Navbar user={plainUser} />
+      {plainUser.role == "user" ? (
+        <UserDashboard />
+      ) : plainUser.role == "admin" ? (
+        <AdminDashboard />
+      ) : (
+        <DeliveryBoy />
+      )}
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { LogOut, Package, Search, ShoppingCartIcon, User } from "lucide-react";
+import {
+  LogOut,
+  Package,
+  Search,
+  ShoppingCartIcon,
+  User,
+  X,
+} from "lucide-react";
 import mongoose from "mongoose";
 import { AnimatePresence, motion } from "motion/react";
 import { signOut } from "next-auth/react";
@@ -18,6 +25,7 @@ interface IUser {
 const Navbar = ({ user }: { user: IUser }) => {
   const plainUser = JSON.parse(JSON.stringify(user));
   const [open, setOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
   const profileDropDown = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
@@ -29,7 +37,9 @@ const Navbar = ({ user }: { user: IUser }) => {
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
-    return document.removeEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
   return (
     <div className="w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-14 px-4 md:px-8 z-50">
@@ -49,7 +59,14 @@ const Navbar = ({ user }: { user: IUser }) => {
           className="w-full outline-none text-gray-700 placeholder-gray-400"
         />
       </form>
+
       <div className="flex items-center gap-3 md:gap-6 relative">
+        <div
+          className="bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition-all md:hidden"
+          onClick={() => setSearchOpen((prev) => !prev)}
+        >
+          <Search className="w-6 h-6 text-green-600" />
+        </div>
         <Link
           href={"/cart"}
           className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition"
@@ -64,11 +81,11 @@ const Navbar = ({ user }: { user: IUser }) => {
           <div
             className="bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform cursor-pointer"
             onClick={() => setOpen((prev) => !prev)}
-            title={user?.name}
+            title={plainUser?.name}
           >
-            {user?.image ? (
+            {plainUser?.image ? (
               <Image
-                src={user?.image}
+                src={plainUser?.image}
                 className="object-cover rounded-full"
                 alt="user"
                 fill
@@ -88,9 +105,9 @@ const Navbar = ({ user }: { user: IUser }) => {
               >
                 <div className="flex items-center gap-3 px-3 py-2 border-b border-gray-100">
                   <div className="w-10 h-10 relative rounded-full bg-gray-100 flex items-center justify-center overflow-hidden">
-                    {user?.image ? (
+                    {plainUser?.image ? (
                       <Image
-                        src={user?.image}
+                        src={plainUser?.image}
                         className="object-cover rounded-full"
                         alt="user"
                         fill
@@ -101,10 +118,10 @@ const Navbar = ({ user }: { user: IUser }) => {
                   </div>
                   <div>
                     <div className="text-gray-800 font-semibold capitalize">
-                      Name: {user?.name}
+                      Name: {plainUser?.name}
                     </div>
                     <div className="text-xs text-gray-500 capitalize">
-                      Role: {user?.role}
+                      Role: {plainUser?.role}
                     </div>
                   </div>
                 </div>
@@ -125,6 +142,29 @@ const Navbar = ({ user }: { user: IUser }) => {
                 >
                   <LogOut className="w-5 h-5 text-red-600" />
                   Log Out
+                </button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {searchOpen && (
+              <motion.div
+                initial={{ opacity: 0, y: 10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                transition={{ duration: 0.6 }}
+                exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                className="fixed top-20 left-1/2 -translate-x-1/2 w-[90%] bg-white rounded-full shadow-lg z-40 flex items-center px-4 py-2"
+              >
+                <Search className="text-gray-500 w-5 h-5 mr-2" />
+                <form className="grow">
+                  <input
+                    type="text"
+                    className="w-full outline-none text-gray-700"
+                    placeholder="Search groceries..."
+                  />
+                </form>
+                <button onClick={()=>setSearchOpen(false)}>
+                  <X className="text-gray-500 w-5 h-5" />
                 </button>
               </motion.div>
             )}
