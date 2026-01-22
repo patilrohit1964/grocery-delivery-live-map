cbbae1f22e03c9ba8ae4dc09396eecd83ff59070
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-22 11:32:36 +0530

    add hamburger responsive ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,13 +52,80 @@ const Navbar = ({ user }: { user: IUser }) => {
     ? createPortal(
         <AnimatePresence>
           <motion.div
-            initial={{ x: 100, opacity: 0 }}
+            initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.6 }}
-            exit={{ x: 100, opacity: 0 }}
-            className="relative z-50 bg-white h-screen w-full"
+            // this type help to add animation on our element,damping help us to control bounce of type animation
+            transition={{ type: "spring", stiffness: 100, damping: 14 }}
+            exit={{ x: -100 }}
+            className="fixed top-0  left-0 h-full w-[75%] sm:w-[60%] z-999 bg-linear-to-b from-green-800/90 via-green-700/80 to-green-900/90 backdrop-blur-xl border-r border-green-400/20 shadow-[0_0_50px_-10px_rgba(0,255,100,0.3)] flex flex-col p-6 text-white"
           >
-            <div></div>
+            <div className="flex items-center justify-between mb-2">
+              <h1 className="font-extrabold text-2xl tracking-wide text-white/90">
+                Admin Panel
+              </h1>
+              <button
+                onClick={() => setMenuOpen(false)}
+                className="text-white/80 hover:text-red-400 text-2xl font-bold transition cursor-pointer"
+              >
+                <X className="w-5 h-5" />
+              </button>
+            </div>
+            {/* user admin profile */}
+            <div className="flex items-center gap-3 p-3 mt-3 rounded-xl bg-white/10 hover:bg-white/15 transition-all shadow-inner ">
+              <div className="flex items-center gap-3 px-3 py-2">
+                {/* user admin image */}
+                <div className="w-12 h-12 relative rounded-full overflow-hidden border-2 border-green-400/60 shadow-lg">
+                  {user?.image ? (
+                    <Image
+                      src={user?.image}
+                      className="object-cover rounded-full"
+                      alt="user"
+                      fill
+                    />
+                  ) : (
+                    <User />
+                  )}
+                </div>
+                {/* user admin name and role */}
+                <div>
+                  <h2 className="text-lg font-semibold text-white capitalize">
+                    Name: {user?.name}
+                  </h2>
+                  <p className="text-xs capitalize tracking-wide text-gray-200">
+                    Role: {user?.role}
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* admin options buttons */}
+            <div className="flex flex-col gap-3 font-medium mt-6">
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <PlusCircle className="w-5 h-5" />
+                Add Grocery
+              </Link>
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <Boxes className="w-5 h-5" />
+                View Grocery
+              </Link>
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <ClipboardCheckIcon className="w-5 h-5" />
+                Manage Orders
+              </Link>
+            </div>
+            <div className="my-5 border-t border-white/20"></div>
+            <div className="flex items-center gap-3 text-red-300 font-semibold mt-auto hover:bg-red-500/20 p-3 rounded-lg transition-all">
+              <LogOut />
+              Log Out
+            </div>
           </motion.div>
         </AnimatePresence>,
         document.body,
