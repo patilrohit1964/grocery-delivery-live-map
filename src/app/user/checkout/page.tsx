d286e12383553163ab46927d1050d7405b7bc49f
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-29 19:29:38 +0530

    working on checkout ui

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, MapPin } from "lucide-react";
 import { motion } from "motion/react";
 import Link from "next/link";
 
@@ -14,9 +14,69 @@ const Checkout = () => {
         <ArrowLeft size={20} />
         <span className="hidden md:inline">Back To Cart</span>
       </Link>
-      <motion.div className="shadow-md rounded-2xl p-4">
-        
-      </motion.div>
+      <motion.h2
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}
+        className="text-2xl sm:text-3xl md:text-4xl font-bold text-green-700 text-center mb-10"
+      >
+        Checkout
+      </motion.h2>
+      <div className="flex justify-between items-center">
+        <div className="shadow-md rounded-2xl p-4 ">
+          <h3 className="flex gap-2 font-medium">
+            <MapPin className="text-green-700" /> Delivery Address
+          </h3>
+          {/* all user details input */}
+          <div>
+            <div className="">
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="Your Name"
+              />
+            </div>
+            <div className="">
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="Your Phone Number"
+              />
+            </div>
+            <div className="">
+              <textarea
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="Your Address"
+              />
+            </div>
+            <div className="flex gap-2 items-center">
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="City"
+              />
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="state"
+              />
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder="pincode"
+              />
+            </div>
+            <div className="">
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder=""
+              />
+            </div>
+            <div className="">
+              <input
+                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                placeholder=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="shadow-md rounded-2xl p-4 ">dd</div>
+      </div>
     </div>
   );
 };
