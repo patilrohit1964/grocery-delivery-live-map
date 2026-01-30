c4bed49d1277e2dbb6f7dd98a3753d2ed86477fa
patilrohit1964 <patilrohit19649@gmail.com>
2026-01-30 18:59:07 +0530

    work on responsive checkout

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -2,8 +2,10 @@
 import { ArrowLeft, CreditCard, MapPin, Truck } from "lucide-react";
 import { motion } from "motion/react";
 import Link from "next/link";
+import { useState } from "react";
 
 const Checkout = () => {
+  const [select, setSelect] = useState(1);
   return (
     <div className="w-[95%] sm:w-[90%] md:w-[80%] mx-auto mt-8 relative">
       <Link
@@ -22,14 +24,14 @@ const Checkout = () => {
       >
         Checkout
       </motion.h2>
-      <div className="flex gap-8">
+      <div className="flex flex-col lg:flex-row gap-8">
         {/* user details div */}
-        <div className="shadow-md rounded-2xl p-4 ">
+        <div className="shadow-md rounded-2xl p-4">
           <h3 className="flex gap-2 font-medium ">
             <MapPin className="text-green-700" /> Delivery Address
           </h3>
           {/* all user details input */}
-          <div className="grid gap-4 my-4">
+          <div className=" w-ful my-4">
             {/* div 1 */}
             <div className="">
               <input
@@ -38,7 +40,7 @@ const Checkout = () => {
               />
             </div>
             {/* div 2 */}
-            <div className="">
+            <div className="my-4">
               <input
                 className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
                 placeholder="Your Phone Number"
@@ -52,7 +54,7 @@ const Checkout = () => {
               />
             </div>
             {/* div 4 */}
-            <div className="flex gap-2 items-center">
+            <div className="flex flex-col md:flex-row gap-2 items-center my-4">
               <input
                 className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
                 placeholder="City"
@@ -67,17 +69,20 @@ const Checkout = () => {
               />
             </div>
             {/* div 5 */}
-            <div className="flex">
+            <div className="flex flex-col md:flex-row mb-4">
               <input
-                className="flex-1 gap-2 items-center rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                className="md:flex-1 gap-2 items-center rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all mb-2 md:mb-0"
                 placeholder="Search city or area"
               />
-              <button className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-6 rounded-lg">
+              <motion.button
+                whileTap={{ scale: 0.8 }}
+                className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-6 py-2 rounded-lg ml-1"
+              >
                 Search
-              </button>
+              </motion.button>
             </div>
             {/* div 6 */}
-            <div className="">
+            <div className="overflow-hidden">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7442.5726911278025!2d72.78280326957798!3d21.140999856772844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1769749165807!5m2!1sen!2sin"
                 width="600"
@@ -91,27 +96,29 @@ const Checkout = () => {
           </div>
         </div>
         {/* checkout div */}
-        <div className="shadow-md rounded-2xl p-4 w-full">
+        <div className="shadow-md rounded-2xl p-4 w-full max-w-full h-96">
           <h3 className="flex gap-2 font-medium items-center ">
             <CreditCard className="text-green-700" /> Payment Method
           </h3>
           <div className="flex flex-col">
             <motion.button
               whileTap={{ scale: 0.8 }}
-              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border border-gray-600 hover:bg-green-100 flex items-center gap-2`}
+              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border-2 border-gray-600 hover:bg-green-100 flex items-center gap-2 ${select === 1 ? "bg-green-100 border-green-600" : ""}`}
+              onClick={() => setSelect(1)}
             >
               <CreditCard className="text-green-700" />
               Pay Online (Stripe)
             </motion.button>
             <motion.button
               whileTap={{ scale: 0.8 }}
-              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border border-gray-600 hover:bg-green-100 flex items-center gap-2`}
+              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border-2 border-gray-600 hover:bg-green-100 flex items-center gap-2 ${select === 2 ? "bg-green-100 border-green-600" : ""}`}
+              onClick={() => setSelect(2)}
             >
               <Truck className="text-green-700" />
               Cash On Delivery
             </motion.button>
           </div>
-          <hr className="mt-4"/>
+          <hr className="mt-4 text-gray-400" />
           <div className="flex flex-col gap-2 py-2">
             <div className="flex justify-between">
               <span className="text-gray-600">Subtotal</span>
@@ -122,7 +129,7 @@ const Checkout = () => {
               <span className="font-semibold text-green-700">₹40</span>
             </div>
           </div>
-          <hr />
+          <hr className="text-gray-400" />
           <div className="flex justify-between border-t border-gray-200 pt-3 text-lg sm:text-xl">
             <span className="font-semibold">Total</span>
             <span className="font-bold text-green-700">₹4000</span>
