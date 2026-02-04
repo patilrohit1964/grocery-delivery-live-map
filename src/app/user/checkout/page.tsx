8dbacc6b0f04469e1a80dffb5dde37b3a20c8c7d
patilrohit1964 <patilrohit19649@gmail.com>
2026-02-04 23:27:10 +0530

    work on checkout

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -1,146 +1,168 @@
 "use client";
-import { ArrowLeft, CreditCard, MapPin, Truck } from "lucide-react";
+import { RootState } from "@/redux/store";
+import {
+  ArrowLeft,
+  Building,
+  Home,
+  MapPin,
+  Navigation,
+  Phone,
+  Search,
+  User,
+} from "lucide-react";
 import { motion } from "motion/react";
 import Link from "next/link";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
   const [select, setSelect] = useState(1);
+  const { userData } = useSelector((state: RootState) => state.user);
+  console.log(userData, "data");
+  const [address, setAddress] = useState({
+    fullName: userData?.name,
+    mobile: userData?.mobile,
+    city: "",
+    state: "",
+    pincode: "",
+    fullAddress: "",
+  });
   return (
-    <div className="w-[95%] sm:w-[90%] md:w-[80%] mx-auto mt-8 relative">
-      <Link
-        href={"/"}
-        className="absolute -top-2 left-0 flex items-center gap-2 text-green-700 font-medium hover:bg-green-800 transition-all px-4 py-2 rounded-full shadow-md hover:shadow-lg duration-300 hover:text-white"
-      >
-        {/* className="absolute top-6 left-6 flex items-center gap-2 text-green-700 font-semibold bg-white px-4 py-2 rounded-full shadow-md hover:bg-green-100 hover:shadow-lg transition-all" */}
-        <ArrowLeft size={20} />
-        <span className="hidden md:inline">Back To Cart</span>
+    <div className="w-[95%] md:w-[80%] mx-auto py-10 relative">
+      <Link href={"/"}>
+        <motion.button
+          whileTap={{ scale: 0.97 }}
+          className="absolute top-0 left-0 flex items-center gap-2 text-green-700 font-semibold hover:text-green-800 cursor-pointer"
+        >
+          <ArrowLeft size={16} />
+          <span className="hidden md:inline">Back To Cart</span>
+        </motion.button>
       </Link>
       <motion.h2
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
-        className="text-2xl sm:text-3xl md:text-4xl font-bold text-green-700 text-center mb-10"
+        className="text-3xl md:text-3xl font-bold text-green-700 text-center mb-10"
       >
         Checkout
       </motion.h2>
-      <div className="flex flex-col lg:flex-row gap-8">
-        {/* user details div */}
-        <div className="shadow-md rounded-2xl p-4">
-          <h3 className="flex gap-2 font-medium ">
+      <div className="grid md:grid-cols-2 gap-8">
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+          className="bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 p-6 border-gray-100"
+        >
+          <h2 className="text-xl font-semibold text-gray-800 mb-4 flex items-center gap-2">
             <MapPin className="text-green-700" /> Delivery Address
-          </h3>
-          {/* all user details input */}
-          <div className=" w-ful my-4">
-            {/* div 1 */}
-            <div className="">
+          </h2>
+          <div className="space-y-4">
+            {/* full name input */}
+            <div className="relative">
+              <User
+                className="absolute left-3 top-3 text-green-600"
+                size={18}
+              />
               <input
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
                 placeholder="Your Name"
+                value={address?.fullName}
+                onChange={(e) =>
+                  setAddress({ ...address, fullName: e.target.value })
+                }
               />
             </div>
-            {/* div 2 */}
-            <div className="my-4">
+            {/* phone input */}
+            <div className="relative">
+              <Phone
+                className="absolute left-3 top-3 text-green-600"
+                size={18}
+              />
               <input
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+                className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
                 placeholder="Your Phone Number"
+                value={address?.mobile}
+                onChange={(e) =>
+                  setAddress({ ...address, mobile: e.target.value })
+                }
               />
             </div>
-            {/* div 3 */}
-            <div className="">
-              <textarea
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
+            {/* address input */}
+            <div className="relative">
+              <Home
+                className="absolute left-3 top-3 text-green-600"
+                size={18}
+              />
+              <input
+                className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
                 placeholder="Your Address"
+                value={address?.fullAddress}
+                onChange={(e) =>
+                  setAddress({ ...address, fullAddress: e.target.value })
+                }
               />
             </div>
-            {/* div 4 */}
-            <div className="flex flex-col md:flex-row gap-2 items-center my-4">
-              <input
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
-                placeholder="City"
-              />
-              <input
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
-                placeholder="state"
-              />
-              <input
-                className="w-full rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all"
-                placeholder="pincode"
-              />
+            {/* city,state,pincode inputs */}
+            <div className="grid grid-cols-3 gap-3">
+              <div className="relative">
+                <Building
+                  className="absolute left-3 top-3 text-green-600"
+                  size={18}
+                />
+                <input
+                  className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
+                  placeholder="Your City"
+                  value={address?.city}
+                  onChange={(e) =>
+                    setAddress({ ...address, city: e.target.value })
+                  }
+                />
+              </div>
+              <div className="relative">
+                <Navigation
+                  className="absolute left-3 top-3 text-green-600"
+                  size={18}
+                />
+                <input
+                  className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
+                  placeholder="Your state"
+                  value={address?.state}
+                  onChange={(e) =>
+                    setAddress({ ...address, state: e.target.value })
+                  }
+                />
+              </div>
+              <div className="relative">
+                <Search
+                  className="absolute left-3 top-3 text-green-600"
+                  size={18}
+                />
+                <input
+                  className="pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50"
+                  placeholder="Your Pincode"
+                  value={address?.pincode}
+                  onChange={(e) =>
+                    setAddress({ ...address, pincode: e.target.value })
+                  }
+                />
+              </div>
             </div>
-            {/* div 5 */}
-            <div className="flex flex-col md:flex-row mb-4">
+            {/* search bar input */}
+            <div className="flex gap-2 mt-3">
               <input
-                className="md:flex-1 gap-2 items-center rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-green-400 border border-gray-300 transition-all mb-2 md:mb-0"
+                className="flex-1 border rounded-lg p-3 text-sm focus:ring-2 focus:ring-green-500 outline-none"
                 placeholder="Search city or area"
+                value={address?.fullAddress}
+                onChange={(e) =>
+                  setAddress({ ...address, fullAddress: e.target.value })
+                }
               />
-              <motion.button
-                whileTap={{ scale: 0.8 }}
-                className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-6 py-2 rounded-lg ml-1"
-              >
+              <button className="bg-green-600 text-white px-5 rounded-lg hover:bg-green-700 transition-all font-medium cursor-pointer">
                 Search
-              </motion.button>
-            </div>
-            {/* div 6 */}
-            <div className="overflow-hidden">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7442.5726911278025!2d72.78280326957798!3d21.140999856772844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1769749165807!5m2!1sen!2sin"
-                width="600"
-                height="450"
-                // style="border:0;"
-                allowFullScreen={false}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
+              </button>
             </div>
           </div>
-        </div>
-        {/* checkout div */}
-        <div className="shadow-md rounded-2xl p-4 w-full max-w-full h-96">
-          <h3 className="flex gap-2 font-medium items-center ">
-            <CreditCard className="text-green-700" /> Payment Method
-          </h3>
-          <div className="flex flex-col">
-            <motion.button
-              whileTap={{ scale: 0.8 }}
-              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border-2 border-gray-600 hover:bg-green-100 flex items-center gap-2 ${select === 1 ? "bg-green-100 border-green-600" : ""}`}
-              onClick={() => setSelect(1)}
-            >
-              <CreditCard className="text-green-700" />
-              Pay Online (Stripe)
-            </motion.button>
-            <motion.button
-              whileTap={{ scale: 0.8 }}
-              className={`w-full mt-4 py-3 px-4 rounded-lg font-medium transition-all cursor-pointer border-2 border-gray-600 hover:bg-green-100 flex items-center gap-2 ${select === 2 ? "bg-green-100 border-green-600" : ""}`}
-              onClick={() => setSelect(2)}
-            >
-              <Truck className="text-green-700" />
-              Cash On Delivery
-            </motion.button>
-          </div>
-          <hr className="mt-4 text-gray-400" />
-          <div className="flex flex-col gap-2 py-2">
-            <div className="flex justify-between">
-              <span className="text-gray-600">Subtotal</span>
-              <span className="font-semibold text-green-700">₹3860</span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-gray-600">Delivery Fee</span>
-              <span className="font-semibold text-green-700">₹40</span>
-            </div>
-          </div>
-          <hr className="text-gray-400" />
-          <div className="flex justify-between border-t border-gray-200 pt-3 text-lg sm:text-xl">
-            <span className="font-semibold">Total</span>
-            <span className="font-bold text-green-700">₹4000</span>
-          </div>
-          <motion.button
-            whileTap={{ scale: 0.8 }}
-            className={`w-full mt-4 py-3 px-4 rounded-full font-medium transition-all cursor-pointer bg-green-600 hover:bg-green-700 text-white`}
-          >
-            Place Order
-          </motion.button>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
