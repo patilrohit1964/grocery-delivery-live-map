30743e29e593b188259b6b4cf7046b9d9c8fff07
patilrohit1964 <patilrohit19649@gmail.com>
2026-02-12 23:16:47 +0530

    working on live order tracking delivery boys

--- grocery-fr/src/app/api/admin/update-order-status/[orderId]/route.ts
+++ grocery-fr/src/app/api/admin/update-order-status/[orderId]/route.ts
@@ -1,5 +1,7 @@
 import connectDb from "@/lib/db";
+import DeliverAssignment from "@/models/deliveryAssignment";
 import Order from "@/models/order.model";
+import User from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(
   req: NextRequest,
@@ -9,7 +11,10 @@ export async function POST(
     await connectDb();
     const { orderId } = await params;
     const { status } = await req.json();
-    const order = await Order.findById(orderId).populate("user");
+    const order = await Order.findById(orderId).populate(
+      "user",
+      "name mobile role image",
+    );
     if (!order) {
       return NextResponse.json(
         {
@@ -23,7 +28,28 @@ export async function POST(
     let availableDeliveryBoys: any = [];
     if (status === "out of delivery" && !order.assignment) {
       const { latitude, longitude } = order.address;
-      
+      // use this operator for live location tracking
+      const nearByDeliveryBoys = await User.find({
+        role: "deliverBoy",
+        location: {
+          $near: {
+            $geometry: {
+              type: "Point",
+              coordinates: [longitude, latitude],
+            },
+            $maxDistance: 10000,
+          },
+        },
+      });
+      const nearByIds = nearByDeliveryBoys.map((db) => db._id);
+      const busyIds = await DeliverAssignment.find({
+        assignTo: { $in: nearByIds },
+        status: { $nin: ["broadcasted", "completed"] },
+      }).distinct("assignTo"); //this distinct return particular field data only
+      const busyIdSet = new Set(busyIds.map((b) => b.toString()));
+      const availableDeliveryBoys = nearByDeliveryBoys.filter(
+        (db) => !busyIdSet.has(String(db._id)),
+      );
     }
   } catch (error) {
     console.log(error, "something went wrong");
