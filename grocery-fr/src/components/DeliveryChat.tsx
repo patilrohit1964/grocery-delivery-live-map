3f9c9a89bf9b94a5c10bd14423cf235b7e7366d3
patilrohit1964 <patilrohit19649@gmail.com>
2026-02-26 23:48:27 +0530

    work on chat live

--- grocery-fr/src/components/DeliveryChat.tsx
+++ grocery-fr/src/components/DeliveryChat.tsx
@@ -23,11 +23,11 @@ const DeliveryChat = ({ orderId, deliveryBoyId }: IProps) => {
         const { data } = await axios.post(`/api/chat/messages`, {
           roomId: orderId,
         });
-        console.log(data, "data");
         if (!data.success) {
           toast.error(data.message || "error while getting message");
           return;
         }
+        toast.success(data.message || "error while getting message");
       } catch (error) {
         console.log(error, "whlile fetching get all messages");
       }
@@ -51,17 +51,33 @@ const DeliveryChat = ({ orderId, deliveryBoyId }: IProps) => {
   };
   return (
     <div className="bg-white rounded-3xl shadow-lg border p-4 h-107.5 flex flex-col">
-      <AnimatePresence>
-        {messages?.map((msg, idx) => (
-          <motion.div
-            key={msg._id?.toString()}
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.2 }}
-            className=""
-          ></motion.div>
-        ))}
-      </AnimatePresence>
+      <div className="flex-1 overflow-y-auto p-2 space-y-3">
+        <AnimatePresence>
+          {messages?.map((msg, idx) => {
+            const isCurrentUser =
+              msg.senderId.toString() === deliveryBoyId.toString();
+              console.log(isCurrentUser)
+            return (
+              <motion.div
+                key={msg._id?.toString()}
+                initial={{ opacity: 0, y: 15 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.2 }}
+                className={`flex ${isCurrentUser ? "justify-end" : "justify-start"}`}
+              >
+                <div
+                  className={`px-4 py-2 max-w-[75%] rounded-2xl shadow ${isCurrentUser ? "bg-green-600 text-white rounded-br-none" : "bg-gray-100 text-gray-800 rounded-bl-none"}`}
+                >
+                  <p>{msg.text}</p>
+                  <p className="text-[10px] opacity-70 mt-1 text-right">
+                    {msg.time}
+                  </p>
+                </div>
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
+      </div>
       <div className="flex gap-2 mt-3 border-t pt-3">
         <input
           type="text"
