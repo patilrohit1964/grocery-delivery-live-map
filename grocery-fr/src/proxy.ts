0f92b710e4fdf0a18902c61abe98e8e761ac1ed2
patilrohit1964 <patilrohit19649@gmail.com>
2026-04-26 11:51:58 +0530

    final deployment

--- grocery-fr/src/proxy.ts
+++ grocery-fr/src/proxy.ts
@@ -1,5 +1,5 @@
-import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
 
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
@@ -9,17 +9,14 @@ export async function proxy(req: NextRequest) {
     return NextResponse.next();
   }
 
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET,
-  });
+  const session = await auth();
 
-  if (!token) {
+  if (!session) {
     const loginUrl = new URL("/login", req.url);
     loginUrl.searchParams.set("callbackUrl", req.url);
     return NextResponse.redirect(loginUrl);
   }
-  const role = token.role;
+  const role = session.user?.role;
   if (pathname.startsWith("/user") && role !== "user") {
     return NextResponse.redirect(new URL("/unauthorized", req.url));
   }
